85bef2a5b826bd267fd45414e5af9a7b157b9adb
famdeg <124525423+famdeg@users.noreply.github.com>
2026-02-16 14:39:41 +0100

    Update DDModbus.py

--- src/DDModbus.py
+++ src/DDModbus.py
@@ -1,287 +1,140 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import threading
-import logging
-import socket
-import traceback
 
-def calc_crc(data):
-    crc = 0xFFFF
-    for pos in data:
-        crc ^= pos 
-        for i in range(8):
-            if ((crc & 1) != 0):
-                crc >>= 1
-                crc ^= 0xA001
-            else:
-                crc >>= 1
-    return crc
+import logging,json
 
-#class used to define a structure of several continuous registers
-class RegisterSet:
-	address=0;
-	data=list();
-	
-	def __init__(self,address,data):
-		self.address=address;
-		self.data=data;
+#This class allow to interface with Home Assistant through the MQTT Discovery Protocol
+class Hassio:
+
+	def __init__(self,mqttClient,topicRoot,clientId,discovery_prefix):
 		
-	def __str__(self):
-		return('Reg:'+str(self.address)+' data: '+str(self.data));
-		
-  
-class slaveRequest:
-	FRAME_MIN_LENGTH=0x08;
-	FRAME_MAX_LENGTH=0x100;
-	
-	def __init__(self,data):
 		#logger
-		self.logger = logging.getLogger(__name__)
-		self.valid=False;
-		self.modbusAddress=0;
-		self.modbusFunctionCode=0;
-		self.R_W=False;
-		self.regAddress=0;
-		self.regNb=0;
-		self.data=dict();
+		self.logger = logging.getLogger(__name__);
+		#attribute init
+		self.availabilityTopic=None;
+		#mqttClient instance ref saving
+		self.mqtt=mqttClient;
+		self.topicRoot=topicRoot;
+		self.clientId=clientId;
+		self.discovery_prefix=discovery_prefix;
+		self.device = {
+			"identifiers": [""],
+			"manufacturer": "",
+			"name": ""
+		}
 		
-		#check rough length
-		if ((len(data) > self.FRAME_MAX_LENGTH) or (len(data) < self.FRAME_MIN_LENGTH )):
-			self.logger.warning('Received Frame Length Error');
-			return;
-		#get modbus function code
-		self.modbusFunctionCode=data[1];
-
-		#if modBus feature is READ_ANALOG_HOLDING_REGISTERS
-		if (self.modbusFunctionCode == DDModbus.READ_ANALOG_HOLDING_REGISTERS):
-			#check CRC
-			crc=calc_crc(data[0:6]);
-			if (crc!=0x100*data[7]+data[6]):
-				self.logger.warning('READ_ANALOG_HOLDING_REGISTERS frame CRC error ');
-				return;
-			
-			#save request informations
-			self.valid=True;
-			self.modbusAddress=data[0];
-			self.R_W=True;
-			self.regAddress=0x100*data[2]+data[3];
-			self.regNb=0x100*data[4]+data[5];
-
-			
-		#if modBus feature is WRITE_MULTIPLE_REGISTERS
-		if (self.modbusFunctionCode == DDModbus.WRITE_MULTIPLE_REGISTERS):
-			#get register number
-			regNumber=data[4]*0x100 + data[5];
-
-			#check that byte nb is twice register number
-			if (2*regNumber != data[6]):
-				self.logger.warning('WRITE_MULTIPLE_REGISTERS reg nb inconsistency');
-				return;
-				
-			#calculate waited frame length
-			frameLength=2*regNumber+9;
-
-			#check frame size is not too short according waited frameLength
-			if (frameLength > len(data)):
-				self.logger.warning('WRITE_MULTIPLE_REGISTERS frame too short');
-				return;
-			
-			#check CRC
-			crc=calc_crc(data[0:frameLength-2]);
-			if (crc!=0x100*data[frameLength-1]+data[frameLength-2]):
-				self.logger.warning('WRITE_MULTIPLE_REGISTERS frame CRC error '+hex(crc));
-				return;
-				
-			#save request informations
-			self.valid=True;
-			self.modbusAddress=data[0];
-			self.R_W=False;
-			self.regAddress=0x100*data[2]+data[3];
-			self.regNb=0x100*data[4]+data[5];
-			for i in range(0,self.regNb):
-				self.data[self.regAddress+i]=0x100*data[7+2*i]+data[8+2*i];
-			
+	def setDevice(self,manufacturer,name,id):
+		self.device = {
+			"identifiers": id,
+			"manufacturer": manufacturer,
+			"name": name
+		}
 	
-class DDModbus:
-	ip=None; #serial port id
-	port=None;
-	CLEANING_TIMEOUT=0.1;
-	SLAVE_RX_TIMEOUT=0.5;
-	MASTER_RX_TIMEOUT=2.5;
-	READ_ANALOG_HOLDING_REGISTERS=0x03;
-	WRITE_MULTIPLE_REGISTERS=0x10;
+	def availabilityInfo(self,shortTopic,payload_available,payload_not_available):
+		#availability info saving
+		
+		self.availabilityTopic=self.topicRoot+'/'+shortTopic;
+		self.payload_available=payload_available;
+		self.payload_not_available=payload_not_available;
 	
-	ANSWER_FRAME_MIN_LENGTH=0x07;
-	ANSWER_FRAME_MAX_LENGTH=0x100;
+	def addSensor(self,object_id,name,deviceClass,shortStateTopic,valueTemplate,unit_of_measurement):
+		#build discovery topic
+		discoveryTopic=self.discovery_prefix+'/sensor/'+self.clientId+'/'+object_id+'/config';
+		#build discovery message payload
+		payload={"name":name};
+		payload["object_id"]=object_id;
+		payload["unique_id"]=self.clientId+'.'+object_id;
+		if (deviceClass is not None):
+			payload["device_class"]=deviceClass;
+		payload["state_topic"]=self.topicRoot+'/'+shortStateTopic;
+		if (valueTemplate is not None):
+			payload["value_template"]=valueTemplate;
+		payload["availability_topic"]=self.availabilityTopic;
+		payload["payload_available"]=self.payload_available;
+		payload["payload_not_available"]=self.payload_not_available;
+		if (unit_of_measurement is not None):
+			payload["unit_of_measurement"]=unit_of_measurement;
+		payload['device'] = self.device
+		#send discovery message
+		self.mqtt.publish(discoveryTopic,json.dumps(payload),1,False);
 
-	def __init__(self,ip,port):
-		#logger
-		self.logger = logging.getLogger(__name__)
+	def addBinarySensor(self,object_id,name,deviceClass,shortStateTopic,payload_on,payload_off):
+		#build discovery topic
+		discoveryTopic=self.discovery_prefix+'/binary_sensor/'+self.clientId+'/'+object_id+'/config';
+		#build discovery message payload
+		payload={"name":name};
+		payload["object_id"]=object_id;
+		payload["unique_id"]=self.clientId+'.'+object_id;
+		if (deviceClass is not None):
+			payload["device_class"]=deviceClass;
+		payload["state_topic"]=self.topicRoot+'/'+shortStateTopic;
+		payload["payload_on"]=payload_on;
+		payload["payload_off"]=payload_off;
+		payload["availability_topic"]=self.availabilityTopic;
+		payload["payload_available"]=self.payload_available;
+		payload["payload_not_available"]=self.payload_not_available;
+		payload["enabled_by_default"]=False;
+		payload['device'] = self.device
+		#send discovery message
+		self.mqtt.publish(discoveryTopic,json.dumps(payload),1,False);
+	
+	def addNumber(self,object_id,name,shortStateTopic,shortCommandTopic,min,max,step,unit_of_measurement):
+		#build discovery topic
+		discoveryTopic=self.discovery_prefix+'/number/'+self.clientId+'/'+object_id+'/config';
+		#build discovery message payload
+		payload={"name":name};
+		payload["object_id"]=object_id;
+		payload["unique_id"]=self.clientId+'.'+object_id;
+		payload["state_topic"]=self.topicRoot+'/'+shortStateTopic;
+		payload["command_topic"]=self.topicRoot+'/'+shortCommandTopic;
+		payload["availability_topic"]=self.availabilityTopic;
+		payload["payload_available"]=self.payload_available;
+		payload["payload_not_available"]=self.payload_not_available;
+		payload["qos"]=2;
+		payload["min"]=min;
+		payload["max"]=max;
+		payload["step"]=step;
+		if (unit_of_measurement is not None):
+			payload["unit_of_measurement"]=unit_of_measurement;
+		payload['device'] = self.device
+		#send discovery message
+		self.mqtt.publish(discoveryTopic,json.dumps(payload),1,False);
 		
-		#socket definition and connection
-		self.ip=ip;
-		self.port=port;
-		self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-		self.socket.connect((self.ip,self.port));
-		
-	def clean(self):
-		run= True;
-		while run:
-			try:
-				self.socket.settimeout(DDModbus.CLEANING_TIMEOUT);
-				data=self.socket.recv(1024);
-				self.logger.debug('Cleaning of: '+str(len(data))+' bytes(s)');
-			except socket.error as exc:
-				run=False;
+	def addSelect(self,object_id,name,shortStateTopic,shortCommandTopic,options):
+		#build discovery topic
+		discoveryTopic=self.discovery_prefix+'/select/'+self.clientId+'/'+object_id+'/config';
+		#build discovery message payload
+		payload={"name":name};
+		payload["object_id"]=object_id;
+		payload["unique_id"]=self.clientId+'.'+object_id;
+		payload["state_topic"]=self.topicRoot+'/'+shortStateTopic;
+		payload["command_topic"]=self.topicRoot+'/'+shortCommandTopic;
+		payload["availability_topic"]=self.availabilityTopic;
+		payload["payload_available"]=self.payload_available;
+		payload["payload_not_available"]=self.payload_not_available;
+		payload["qos"]=2;
+		payload["options"]=options;
+		payload['device'] = self.device
+		#send discovery message
+		self.mqtt.publish(discoveryTopic,json.dumps(payload),1,False);	
 
-	def slaveRx(self,modbusSlaveAddress):
-			try:
-				self.socket.settimeout(DDModbus.SLAVE_RX_TIMEOUT);
-				data=self.socket.recv(1024);
-				self.logger.debug('Frame received: '+data.hex());
-				
-				#frame consistency check
-				frame=slaveRequest(data);
-				
-				#if slave address is correct
-				if ((modbusSlaveAddress!=0) and (frame.modbusAddress==modbusSlaveAddress)):
-					#ack for WRITE_MULTIPLE_REGISTERS request
-					if (frame.modbusFunctionCode==DDModbus.WRITE_MULTIPLE_REGISTERS):
-						self.logger.debug('Ack WRITE_MULTIPLE_REGISTERS frame');
-						tx=bytearray();
-						tx.extend(data[0:6]);
-						crc=calc_crc(tx);
-						tx.append(crc & 0xFF);
-						tx.append((crc >> 8) & 0xFF);
-						tx.append(0);
-						self.socket.send(tx);
-					#ack for READ_ANALOG_HOLDING_REGISTERS
-					elif (frame.modbusFunctionCode==DDModbus.READ_ANALOG_HOLDING_REGISTERS):
-						#it is not possible to ack the request as there is not content to provide
-						self.logger.debug('No Ack for READ_ANALOG_HOLDING_REGISTERS frame');
-
-				return frame;
-			except socket.error as exc:
-				return False;
-				
-	def masterReadAnalog(self,modbusAddress,regAddress,regNb):
-		
-		#build request
-		request=bytearray();
-		request.append(modbusAddress);
-		request.append(DDModbus.READ_ANALOG_HOLDING_REGISTERS);
-		request.append((regAddress>>8)& 0xFF);
-		request.append(regAddress & 0xFF);
-		request.append((regNb>>8)& 0xFF);
-		request.append(regNb & 0xFF);
-		crc=calc_crc(request);
-		request.append(crc & 0xFF);
-		request.append((crc>>8)& 0xFF);
-		request.append(0);
-		
-		#send it
-		self.logger.debug('Send read request: '+request.hex());
-		self.socket.send(request);
-		
-		#wait for answer
-		try:
-			self.socket.settimeout(DDModbus.MASTER_RX_TIMEOUT);
-			answer=self.socket.recv(1024);
-			self.logger.debug('Answer received: '+answer.hex());
-			
-			#check answer
-			
-			#check rough length
-			if ((len(answer) > self.ANSWER_FRAME_MAX_LENGTH) or (len(answer) < self.ANSWER_FRAME_MIN_LENGTH )):
-				self.logger.warning('Rough Answer Length Error');
-				return;
-		
-			#check  modBus address
-			if (answer[0] != modbusAddress):
-				self.logger.warning('Answer modbus address Error');
-				return;
-			
-			#check  modBus feature
-			if (answer[1] != DDModbus.READ_ANALOG_HOLDING_REGISTERS):
-				self.logger.warning('Answer modbus feature Error');
-				return;
-				
-			#check byte nb
-			if (answer[2] != 2*regNb):
-				self.logger.warning('Answer byte number Error');
-				return;
-				
-			#check length
-			answerLength=5+answer[2];
-			if ((len(answer) < answerLength )):
-				self.logger.warning('Answer Length Error');
-				return;
-				
-			#check CRC
-			crc=calc_crc(answer[0:answerLength-2]);
-			if (crc!=0x100*answer[answerLength-1]+answer[answerLength-2]):
-				self.logger.warning('Answer CRC error ');
-				return;
-			self.logger.debug('Answer valid ');
-			
-			#return answer as dict
-			data=dict();
-			for i in range(0,regNb):
-				data[regAddress+i]=0x100*answer[3+2*i]+answer[4+2*i];
-			return(data);
-			
-		except socket.error as exc:
-			self.logger.warning('No answer to masterReadAnalog');
-			return;
-			
-	def masterWriteAnalog(self,modbusAddress,regAddress,data):
-		#build request
-		request=bytearray();
-		#byte 0
-		request.append(modbusAddress);
-		#byte 1
-		request.append(DDModbus.WRITE_MULTIPLE_REGISTERS);
-		#byte 2 & 3
-		request.append((regAddress>>8)& 0xFF);
-		request.append(regAddress & 0xFF);
-		#byte 4 & 5 Reg Nb
-		request.append(0);
-		request.append(len(data));
-		#byte 6 byte Nb
-		request.append(2*len(data));
-		#data
-		for reg in data:
-			request.append((reg>>8)& 0xFF);
-			request.append(reg & 0xFF);
-			
-		crc=calc_crc(request);
-		request.append(crc & 0xFF);
-		request.append((crc>>8)& 0xFF);
-		request.append(0);
-		
-		#send it
-		self.logger.info('Send write request: '+request.hex());
-		self.socket.send(request);
-		
-		#wait for ack
-		try:
-			self.socket.settimeout(DDModbus.MASTER_RX_TIMEOUT);
-			answer=self.socket.recv(1024);
-			self.logger.debug('Ack received: '+answer.hex());
-			#check ack
-			waited_ack=request[0:6];
-			crc=calc_crc(waited_ack);
-			waited_ack.append(crc & 0xFF);
-			waited_ack.append((crc>>8)& 0xFF);
-			if (waited_ack==answer[0:8]):
-				self.logger.info('Ack OK');
-				return(True);
-			else:
-				self.logger.warning('Ack KO. Waited Ack was : '+waited_ack.hex());
-				return(False);
-			
-			
-		except socket.error as exc:
-			self.logger.warning('No ack  to master write request');
-			return(False);
+	def addSwitch(self,object_id,name,shortStateTopic,shortCommandTopic,payload_off,payload_on):
+		#build discovery topic
+		discoveryTopic=self.discovery_prefix+'/switch/'+self.clientId+'/'+object_id+'/config';
+		#build discovery message payload
+		payload={"name":name};
+		payload["object_id"]=object_id;
+		payload["unique_id"]=self.clientId+'.'+object_id;
+		if (shortStateTopic is not None):
+			payload["state_topic"]=self.topicRoot+'/'+shortStateTopic;
+		payload["command_topic"]=self.topicRoot+'/'+shortCommandTopic;
+		payload["availability_topic"]=self.availabilityTopic;
+		payload["payload_available"]=self.payload_available;
+		payload["payload_not_available"]=self.payload_not_available;
+		payload["payload_off"]=payload_off;
+		payload["payload_on"]=payload_on;		
+		payload["qos"]=2;
+		payload['device'] = self.device
+		#send discovery message
+		self.mqtt.publish(discoveryTopic,json.dumps(payload),1,False);		
 
